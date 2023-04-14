39519da12ab24ac3834e47e3d5f3220ad8d13d4b
Jazmin Ivy <princessivy07@yahoo.com>
2023-04-14 15:07:22 -0500

    Had to change Text to String to get rid of marshmallow.Fields AttributeError

--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -80,7 +80,7 @@ class FavoriteSchema(ma.Schema):
     id = fields.Integer(primary_key=True)
     book_id = fields.String(required=True)
     title = fields.String(required=True)
-    thumbnail_url = fields.Text()
+    thumbnail_url = fields.String(required=True)
     user_id = fields.Integer()
     user = ma.Nested(UserSchema, many=False)
     class Meta:
