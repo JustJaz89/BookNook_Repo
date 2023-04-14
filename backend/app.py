d7d741f12b4fe6b230289ecbc60facd2860213db
Jazmin Ivy <princessivy07@yahoo.com>
2023-04-14 15:48:55 -0500

    Created a UserFavorites Resource that contains a get and post method with JWT authorizations

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from database.models import db
 from database.schemas import ma
 from resources.auth import LoginResource, RegisterResource
 from resources.cars import AllCarResource, UserCarResource
+from resources.books import UserReviewsResource, UserFavoritesResource
 from dotenv import load_dotenv
 from os import environ
 
@@ -55,6 +56,8 @@ def create_routes():
     api.add_resource(LoginResource, '/api/auth/login')
     api.add_resource(AllCarResource, '/api/cars')
     api.add_resource(UserCarResource, '/api/user_cars')
-    # TODO: Create files for your Resources in resources folder, add them here
-    
+    # TODO: Create files for your Resources in resources folder, add them here 
+    api.add_resource(UserReviewsResource, '/api/user_cars')
+    api.add_resource(UserFavoritesResource, '/api/user_cars')
+
     return api
--- backend/resources/books.py
+++ backend/resources/books.py
@@ -1,16 +1,33 @@
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 from flask_restful import Resource
-from database.models import db, review
+from database.models import db, Review, Favorite
 from database.schemas import review_schema, reviews_schema, favorite_schema, favorties_schema
 
-class UserReviews(Resource):
+class UserReviewsResource(Resource):
     @jwt_required()
     def post(self):
         user_id = get_jwt_identity()
-        form_data = request.get.json()
-        new_review = review.schema.load(form_data)
+        form_data = request.get_json()
+        new_review = review_schema.load(form_data)
         new_review.user_id = user_id
         db.session.add(new_review)
         db.session.commit()
         return review_schema.dump(new_review), 201
+    
+class UserFavoritesResource(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        user_favorites = Favorite.query.filter_by(user_id=user_id)
+        return favorties_schema.dump(user_favorites), 200
+    
+    @jwt_required()
+    def post(self):
+        user_id = get_jwt_identity()
+        form_data = request.get_json()
+        new_favorite = favorite_schema.load(form_data)
+        new_favorite.user_id = user_id
+        db.session.add(new_favorite)
+        db.session.commit()
+        return favorite_schema.dump(new_favorite)
