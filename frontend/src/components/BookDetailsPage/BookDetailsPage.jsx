f48d61bd8d6d0e5e07c8f9c68ef6d8c1e642e455
Jazmin Ivy <princessivy07@yahoo.com>
2023-04-19 15:00:10 -0500

    Updated BookDetailsPage component

--- frontend/src/components/BookDetailsPage/BookDetailsPage.jsx
+++ frontend/src/components/BookDetailsPage/BookDetailsPage.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 const BookDetailsPage = () => {
     const {bookName} = useParams();
     const {bookDetails, setBookDetail} = useState({});
+    const {isLoading, setIsLoading} = useState(true);
 
     useEffect(() => {
         fetchBookDetails();
@@ -12,15 +13,20 @@ const BookDetailsPage = () => {
     const fetchBookDetails = async () => {
         try{
             let response = await axios.get(`${bookName}`);
-        console.log(response.data);
+        setBookDetail(response.data);
+        setIsLoading(False);
         } catch(error){
             console.log("Error in fetchBookDetails:", error)
         }
     };
-    
+
     return (
         <div className="container">
             <h1>{bookName} Details!</h1>
+            {isLoading ? <div>Loading...</div>: 
+            <div>
+                <h3></h3>
+            </div>}
         </div>
     );
 };
